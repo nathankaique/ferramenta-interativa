26a207ba817bbb420b0fc1a84897a323c61183be
nathankaique <119627814+nathankaique@users.noreply.github.com>
2024-10-16 12:28:16 -0300

    Add files via upload

---
+++ ferramenta-interativa-main/perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    "programação",
+    "Quem é o melhor jogador do mundo?",
+    "Cristiano Ronaldo"
+)
+criaCartao(
+    "Geografia",
+    "qual a capital da frança",
+    "a capital da frança é parís"
+)
+criaCartao(
+    "historia",
+    "primeiro campeão da copa do mundo",
+    "Uruguai"
+)
+criaCartao(
+    "geografia",
+    "capital da russia?",
+    "moscou"
+)
